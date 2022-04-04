b98e1b2cc66dbaaec9a5e99f4510255271029211
Saklain-Tonmoy <tonmoysaklain@gmail.com>
2022-04-04 12:53:39 +0600

    Feature - Vanilla Js and Umbrella Js Unit Testing

--- __tests__/fetch.spec.js
+++ __tests__/fetch.spec.js
@@ -5,13 +5,14 @@ const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf8");
 
 jest.dontMock("fs");
 
-json = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../language.json")));
+json = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../language.txt")));
 
 describe("fetch list", function () {
   beforeEach(() => {
     document.documentElement.innerHTML = html.toString();
   });
 
+  // Specifying JEST timeouts for Asynchronous Callback. Default is 5000ms.
   beforeAll(async () => {
     jest.setTimeout(10000);
   });
@@ -29,6 +30,7 @@ describe("fetch list", function () {
 
     getItem()
       .then((res) => {
+        console.log(res);
         expect(res.length).toEqual(json.length);
       })
       .catch((err) => console.log(err));
